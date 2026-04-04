88d5484ab2253e45a33a9c0ecd3a056d8a4a76b3
EzzEldienMohsen <ezzmohsend@gmail.com>
2026-04-04 11:23:43 +0200

    LIP-connect two containers by getting the ips using docker inspect to fetch container infos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
-
+const mongoose = require("mongoose")
 const PORT =process.env.PORT || 4000
+const DB_USERNAME= "root";
+const DB_PASSWORD= "example";
+const DB_PORT=27017;
+const DB_HOST ="172.18.0.3"
+const Uri =`mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`
 console.log("process.env.PORT is ->",process.env.PORT)
 console.log("process.env.NODE_ENV is ->",process.env.NODE_ENV)
 const app = express()
+mongoose.connect(Uri).then(()=> console.log("connected to DB")).catch((error)=> console.log("failed with error ->", error));
 app.get("/",(req,res)=>res.send("<h1 style='background-color: black; color: crimson; padding: 10px; border-radius: 8px;'>hi the server ezz is up & running</h1>"))
 
 app.listen(PORT, ()=> console.log(`server is up and running on PORT: ${PORT}`))
